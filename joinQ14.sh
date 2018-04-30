2286e41b988bba6960c1221c1ff4692542ff812b
primkey7607 <psubramaniam@uchicago.edu>
2018-04-30 19:08:55 +0000

    using different scales

--- joinQ14.sh
+++ joinQ14.sh
@@ -6,8 +6,8 @@ for i in 1 5 10 15 20; do
         cd ~/tpch-generator/dbgen/
         #./dbgen -f -s $i
         echo "scale:$i"
-        for p_key in DICT PLAIN; do
-                for l_key in DICT PLAIN; do
+        for p_key in DICT; do
+                for l_key in DICT; do
                         echo "${p_key},${l_key} part.0 lineitem.1"
                         cd ~/my_encsel/
                         java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.GlobalJoinFileProducer $p_key $l_key UNCOMPRESSED
