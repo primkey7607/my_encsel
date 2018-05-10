c8be84bfda0c25924c1d12309c86eb90715a4c5e
primkey7607 <psubramaniam@uchicago.edu>
2018-05-10 20:21:58 +0000

    doubles not yet working

--- joinQ14.sh
+++ joinQ14.sh
@@ -6,11 +6,11 @@ for i in 1; do
         cd ~/tpch-generator/dbgen/
         #./dbgen -f -s $i
         echo "scale:$i"
-        for p_key in DICT; do
-                for l_key in DICT; do
+        for p_key in PLAIN_DICTIONARY; do
+                for l_key in PLAIN_DICTIONARY; do
                         echo "${p_key},${l_key} part.0 lineitem.1"
                         cd ~/my_encsel/
-                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.GlobalJoinFileProducer $p_key $l_key UNCOMPRESSED
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.DblJoinFileProducer $p_key $l_key UNCOMPRESSED
                         java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.HashJoinTool
                         #cd $BASEDIR
                 done
