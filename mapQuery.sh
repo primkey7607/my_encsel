2e8eb778d9cec1fb01240c7761ce251a17863aab
Pranav <submarine3.14159@gmail.com>
2018-06-18 10:31:50 -0400

    adding appropriate bash file

--- mapQuery.sh
+++ mapQuery.sh
@@ -2,7 +2,7 @@
 # Chunwei Liu
 # RLE BIT_PACKED 
 BASEDIR=$(pwd)
-for i in 1 2 3 4 5; do
+for i in 4 5; do
         cd ~/tpch-generator/dbgen/
         ./dbgen -f -s $i
         echo "scale:$i"
@@ -11,7 +11,8 @@ for i in 1 2 3 4 5; do
                         echo "${p_key},${l_key} part.0 lineitem.1"
                         cd ~/my_encsel/
                         java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.JoinFileProducer $p_key $l_key UNCOMPRESSED
-                        java -Xmx4g -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.HashJoinTool
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.RecodeTool
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.HashJoinTool
                         #cd $BASEDIR
                 done
         done
