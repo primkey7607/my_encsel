26bcb1ecd40e62a6f840313441ed57c248de8ea1
Chameleon Cloud User <cc@rec-1.novalocal>
2018-06-18 15:30:37 +0000

    bash file for comparison with recoding

---
+++ joinR.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+# Chunwei Liu
+# RLE BIT_PACKED 
+BASEDIR=$(pwd)
+for i in 10; do
+        cd ~/tpch-generator/dbgen/
+        #./dbgen -f -s $i
+        echo "scale:$i"
+        for p_key in DICT; do
+                for l_key in DICT; do
+                        echo "${p_key},${l_key} part.0 lineitem.1"
+                        cd ~/my_encsel/
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.JoinFileProducer $p_key $l_key UNCOMPRESSED
+                        java -cp target/enc-selector-0.0.1-SNAPSHOT.jar:target/enc-selector-0.0.1-SNAPSHOT-jar-with-dependencies.jar edu.uchicago.cs.encsel.query.HashJoinTool
+                        #cd $BASEDIR
+                done
+        done
+done
+echo "query ended!"
